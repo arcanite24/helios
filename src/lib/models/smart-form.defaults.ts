10d32ad4d4118f631a9de73b3b93fcdc1c477d16
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-04 15:18:22 +0800

    Delete src directory

--- src/lib/models/smart-form.defaults.ts
+++
@@ -1,9 +0,0 @@
-// Helios Defaults
-export const heliosDefaultSubmitButtonText = 'Submit'
-
-export enum fileUploaderText {
-  Initial = 'Select File',
-  Uploading = 'Uploading File',
-  Finished = 'Finished',
-  Error = 'Error on upload'
-}
--- src/public_api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of helios
- */
-
-/* export * from './lib/helios.service'; */
-export * from './lib/helios.module';
-export * from './lib/models/smart-form.model';
-export * from './lib/shared/smart-form/smart-form.component';
